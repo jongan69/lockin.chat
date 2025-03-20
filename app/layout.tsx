6d688b07c986d27a3f1143a44062bba8628b5b3a
Jonathan Gan <jonathang132298@gmail.com>
2025-03-20 15:37:21 -0400

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { getJupiterPrice } from "./utils/getJupiterPrice";
+import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6202902142885850"
+          crossOrigin="anonymous" />
+      </Head>
       <body className={inter.className}>
         {children}
       </body>
