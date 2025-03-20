472ee5b3f11448ba56b67d5d1a004d65d470cc61
Jonathan Gan <jonathang132298@gmail.com>
2025-03-20 15:44:37 -0400

    ??

--- app/linkpage.tsx
+++ app/linkpage.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import './App.css';
 import LinkTree from './components/LinkTree';
 import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script';
 
 export default function App() {
 
   return (
     <>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6202902142885850"
+        crossOrigin="anonymous" />
       <LinkTree />
-      <Analytics/>
+      <Analytics />
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Script from 'next/script'
 import LinkPage from './linkpage'
 
 export default async function Page() {
-  return <LinkPage />
+  return (
+    <>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6202902142885850"
+        crossOrigin="anonymous" />
+      <LinkPage />
+    </>
+  )
 }
